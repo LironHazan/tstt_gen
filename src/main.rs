567ec5af5d4b2d8bfe1c37e684bfd17581ebfaf3
lironh <lironenk@gmail.com>
2020-09-12 21:42:14 +0300

    feat(main): output path

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::fs::File;
 use std::path::Path;
 use std::fs;
 use serde_derive::{Deserialize, Serialize};
+use crate::test_gen::Suite;
 
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -17,20 +18,26 @@ struct PrivateConfig {
 
 #[tokio::main]
 async fn main()  -> Result<(), std::io::Error> {
-    let config_path = Path::new("config.json");
-    let config_file = File::open(config_path).expect("file not found");
-    let config: PrivateConfig = serde_json::from_reader(config_file).expect("error while reading json");
+    let config = get_config("config.json");
 
     for table in config.tables {
         fs::create_dir(&table)?;
-
-        let path = format!("sheets/tables/{}.json", table);
-        let json_file_path = Path::new(&path);
-        let json_file = File::open(json_file_path).expect("file not found");
-        let suite = serde_json::from_reader(json_file).expect("error while reading json");
-
+        let suite = parsed_tables(&table);
         test_gen::generate_test_suite(suite, format!("{}/{}", config.output_dir,table)).await?;
     }
 
     Ok(())
+}
+// Get config obj
+fn get_config(filename: &str) -> PrivateConfig {
+    let config_path = Path::new(filename);
+    let config_file = File::open(config_path).expect("file not found");
+    return serde_json::from_reader(config_file).expect("error while reading json");
+}
+// Get the parsed tables data based on the tests sheets
+fn parsed_tables(table: &str) -> Vec<Suite> {
+    let path = format!("sheets/tables/{}.json", table);
+    let json_file_path = Path::new(&path);
+    let json_file = File::open(json_file_path).expect("file not found");
+    return serde_json::from_reader(json_file).expect("error while reading json");
 }
